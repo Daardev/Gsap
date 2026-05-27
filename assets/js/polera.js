881140efaecf4457db62aa2e7cd17b369efff345
Daardev <Darwingmunoza@gmail.com>
2026-05-26 22:09:47 -0400

    poleras

--- assets/js/polera.js
+++ assets/js/polera.js
@@ -12,54 +12,37 @@ const currentFrame = (index) => (
   `../assets/frame/frame_${String(index + 1).padStart(4, "0")}.webp`
 );
 
-const images = [];
-
 const sequence = {
   frame: 0
 };
 
-for (let i = 0; i < frameCount; i++) {
-
+const images = [...Array(frameCount)].map((_, i) => {
   const img = new Image();
-
   img.src = currentFrame(i);
-
-  images.push(img);
-}
+  return img;
+});
 
 images[0].onload = render;
 
 function render() {
-
   const img = images[sequence.frame];
-
   if (!img) return;
-
   context.clearRect(0, 0, canvas.width, canvas.height);
-
   const canvasRatio = canvas.width / canvas.height;
   const imageRatio = img.width / img.height;
-
   let drawWidth;
   let drawHeight;
   let x;
   let y;
-
   // COVER STYLE (tipo background-size: cover)
-
   if (imageRatio > canvasRatio) {
-
     drawHeight = canvas.height;
     drawWidth = img.width * (drawHeight / img.height);
-
     x = (canvas.width - drawWidth) / 2;
     y = 0;
-
   } else {
-
     drawWidth = canvas.width;
     drawHeight = img.height * (drawWidth / img.width);
-
     x = 0;
     y = (canvas.height - drawHeight) / 2;
   }
